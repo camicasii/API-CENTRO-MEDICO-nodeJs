025e628830582bddb794b9bae46b69e27d77b1f0
camicasii <camicasii@hotmail.com>
2019-09-26 09:22:02 -0400

    Enviando la app a heroku2

--- src/lib/conrConf.js
+++ src/lib/conrConf.js
@@ -4,7 +4,7 @@ problemas con el acceso CORS en NodeJS y nos fallen las peticiones.*/
 
 const middleware={
     cors:(req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.LOCALHOST);    
+    res.header('Access-Control-Allow-Origin', "*");    
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type , Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
