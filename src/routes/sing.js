327d060d3d8623757212815b3fca937ed2c8be05
camicasii <camicasii@hotmail.com>
2019-09-26 12:36:35 -0400

    debug heroku 2.22

--- src/routes/sing.js
+++ src/routes/sing.js
@@ -14,9 +14,7 @@ router.get('/signout',checkToken, async(req,res)=>{
     const userjwt = await User.find({tokenId:req.headers.authorization})
     if(userjwt!==null||userjwt!==undefined){
     userjwt.tokenId=null;    
-    await User.findByIdAndUpdate(userjwt._id,userjwt)
-    
-    userjwt. userjwt.save().then(()=>{
+    await User.findByIdAndUpdate(userjwt._id,userjwt).then(()=>{
         res.status(200)
     }).catch(()=>res.status(401))
 }
