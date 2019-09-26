d330faba6595915b79895a750bc7ac8fe52af845
camicasii <camicasii@hotmail.com>
2019-09-26 14:05:08 -0400

    debug heroku 2.32

--- src/routes/sing.js
+++ src/routes/sing.js
@@ -10,25 +10,21 @@ router.post('/signup',signup);
 router.post('/signin',signin)
 router.delete('/signout',checkToken, async(req,res)=>{
     console.log("paso out");    
-    try{    
-    const userjwt = await User.findOne({tokenId:req.headers.authorization})
+    
+    const jwUser = await User.findOne({tokenId:req.headers.authorization})
     console.log("busco user");
     console.log(userjwt);        
-    if(userjwt!==null||userjwt!==undefined){
+    if(jwUser!==null||jwUser!==undefined){
         console.log("paso el si");
         
-    userjwt.tokenId="";  
+        jwUser.tokenId=null;  
     console.log("paso el si");
-    console.log(userjwt);
+    console.log(jwUser);
     //res.status(200)
-    await userjwt.save().then(()=>{
-        res.status(200)
-    })
-    
+    await jwUser.save()
+    res.status(200)
     }
-}catch{
-    res.status(401)}
-})
+
 
 
 
